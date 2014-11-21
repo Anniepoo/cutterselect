7e597d1c276c144c34804765b36d63ca882596d1
Anniepoo <annie66us@yahoo.com>
2014-11-21 11:13:44 -0800

    Set up pack info

---
+++ pack.pl
@@ -0,0 +1,7 @@
+author('Anne Ogborn', 'anniepoo@yahoo.com').
+download('https://github.com/Anniepoo/cutterselect.git').
+home('https://github.com/Anniepoo/cutterselect').
+name(cutterselect).
+packager('Anne Ogborn', 'me@wouterbeek.com').
+title('cutterselect').
+version('0.0.0').
